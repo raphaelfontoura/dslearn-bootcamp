5c1b55ae78d4054156f806d5c8e9c25419825834
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-07-06 16:28:51 -0300

    Add Course model and Seed in Data.sql

---
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Course.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslearn.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_course")
+public class Course implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imgUri;
+    private String imgGrayUri;
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,3 +9,6 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT'),('ROLE_INSTRUCTOR'),('RO
 
 INSERT INTO tb_user_role (user_id, role_id)
     VALUES (1,1), (2,1), (2,2), (3,1), (3,2), (3,3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri)
+    VALUES ('Bootcamp HTML', 'https://c.pxhere.com/images/84/de/c46800ed7f9823cc820f44de5898-1575569.jpg!d', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
