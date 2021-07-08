f66d80ef38336d4e62755234c484ac7ed53f17e0
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-07-08 11:27:41 -0300

    Lessons seed. Fix: lessonsDone in Enrollment

--- backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Enrollment.java
@@ -5,6 +5,8 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -23,6 +25,10 @@ public class Enrollment implements Serializable {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    @Setter(AccessLevel.NONE)
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available, boolean onlyUpdate) {
         id.setUser(user);
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -31,3 +31,21 @@ INSERT INTO tb_enrollment (user_id, offer_id ,enroll_moment, refund_moment, avai
     VALUES (1,1,TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z',null,true,false),
         (2,1,TIMESTAMP WITH TIME ZONE '2020-11-20T14:00:00Z',null,true,false);
 
+INSERT INTO tb_lesson (title, position, section_id)
+    VALUES ('Aula 1 do Capítulo 1', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1,'Material de apoio: link','https://youtu.be/sqbqoR-lMf8');
+
+INSERT INTO tb_lesson (title, position, section_id)
+    VALUES ('Aula 2 do Capítulo 1', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2,'','https://youtu.be/sqbqoR-lMf8');
+
+INSERT INTO tb_lesson (title, position, section_id)
+    VALUES ('Aula 3 do Capítulo 1', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3,'','https://youtu.be/sqbqoR-lMf8');
+
+INSERT INTO tb_lesson (title, position, section_id)
+    VALUES ('Tarefa do Capítulo 1', 4, 1);
+INSERT INTO tb_task (id, description, question_count, approval_count, weight, due_date)
+    VALUES (4, 'Fazer um trabalho legal', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-11-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1), (2, 1, 1);
