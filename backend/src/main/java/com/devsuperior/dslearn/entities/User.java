31f7cd414abd8c393cdfd08b04ea189716e1bbc8
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-07-15 17:36:05 -0300

    Oauth security and login

--- backend/src/main/java/com/devsuperior/dslearn/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/User.java
@@ -1,20 +1,21 @@
 package com.devsuperior.dslearn.entities;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +24,7 @@ public class User implements Serializable {
     @Getter @Setter
     private String name;
     @Getter @Setter
+    @Column(unique = true)
     private String email;
     @Getter @Setter
     private String password;
@@ -37,4 +39,36 @@ public class User implements Serializable {
     @OneToMany(mappedBy = "user")
     @Getter
     private List<Notification> notifications = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ backend/src/main/java/com/devsuperior/dslearn/services/UserService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dslearn.services;
+
+import com.devsuperior.dslearn.entities.User;
+import com.devsuperior.dslearn.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByEmail(username);
+        if (user == null) {
+            logger.error("Username not found: " + username);
+            throw new UsernameNotFoundException("Login not found");
+        }
+        logger.info("User found: " + username);
+        return user;
+    }
+}
